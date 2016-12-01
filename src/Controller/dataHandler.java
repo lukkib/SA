84cd4e7ef75f8ba62b9b1863aedb97f01984b172
l.ber <l.ber@student.tugraz.at>
2016-12-01 17:05:58 +0100

    Updated file structure.

---
+++ src/Controller/dataHandler.java
@@ -0,0 +1,67 @@
+package Controller;
+
+import io.CsvReader;
+import io.CsvWriter;
+
+import java.util.*;
+
+/**
+ * Created by paul on 30.11.16.
+ */
+public class dataHandler {
+
+    private List<Stop> stops;
+    private String path;
+
+    public dataHandler() {
+        this.path = System.getProperty("user.dir").concat("/mapping.csv");
+        stops = new ArrayList<Stop>();
+    }
+
+    // Return List of all stops within a route
+    public List<String> getRoutesofStop(String stopId)
+    {
+        CsvReader in = new CsvReader(path, "|");
+        stops = in.readCsv();
+        List<String> res = new ArrayList<String>();
+        for(int i = 0; i < stops.size(); i++)
+        {
+            if(compare(stopId, stops.get(i).getStopId()))
+            {
+                res.add(stops.get(i).getRouteId());
+            }
+        }
+        standardise();
+        return res;
+    }
+
+    public void standardise()
+    {
+        int len;
+        for(int i = 0; i < stops.size(); i++)
+        {
+            stops.get(i).getRouteId().replace("\n", "");
+        }
+
+    }
+
+    //TODO: find a better solution for comparing Strings --- .equals() doesn't work properly
+    public boolean compare(String str1, String str2)
+    {
+        for(int i = 0; i < str1.length() && i < str2.length(); i++)
+        {
+            if(str1.charAt(i) != str2.charAt(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addStopToLine(String line, String stop)
+    {
+        String row = line.concat("|").concat(stop).concat("\n");
+        CsvWriter writer = new CsvWriter(System.getProperty("user.dir").concat("/mapping.csv"));
+        writer.writeLine(row);
+    }
+}
---
+++ src/io/CsvReader.java
@@ -0,0 +1,55 @@
+package io;
+
+import Controller.Stop;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
+public class CsvReader {
+    private String path;
+    private String separator;
+    private List<Stop> stops;
+
+    public CsvReader(String path, String separator) {
+
+        this.path = path;
+        this.separator = separator;
+    }
+
+    public List<Stop> readCsv()
+    {
+        List<Stop> stops = new ArrayList<Stop>();
+        try
+        {
+            //Initialize Scanner
+            Scanner scanner = new Scanner(new File(path));
+            scanner.useDelimiter("\n");
+            StringTokenizer tok;
+            //Skipping first line
+            scanner.next();
+            //Get new line as long as there are more lines
+            while (scanner.hasNext())
+            {
+                //Splitting line into tokens
+                tok = new StringTokenizer(scanner.next(), separator);
+                // Creating and adding a stop to the stops list
+
+                Stop stop = new Stop(tok.nextToken(), tok.nextToken());
+                stops.add(stop);
+            }
+            scanner.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return stops;
+    }
+}
