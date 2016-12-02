97e60cfcca76bfc9dfb3c4624bb24d7ecebd246c
l.ber <l.ber@student.tugraz.at>
2016-12-02 15:46:25 +0100

    Gui dependencies fixed.

--- src/main/java/at/tugraz/sa/gui/Gui.java
+++ src/main/java/at/tugraz/sa/gui/Gui.java
@@ -10,12 +10,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+import java.net.URL;
 
 public class Gui extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+    public void start(Stage stage) throws Exception
+    {
+        String path = "src/main/java/at/tugraz/sa/gui/FXMLDocument.fxml";
+        URL url = new File(path).toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
